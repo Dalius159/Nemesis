8190cfe034eaf4db489aeefa47f8d6eee4f9d2e6
Dalius <nhat.nm207645@sis.hust.edu.vn>
2024-05-04 15:35:54 +0700

    first commit

---
+++ addons/website_airproof/__manifest__.py
@@ -0,0 +1,24 @@
+{
+   'name': 'Airproof Theme',
+   'category': 'Website/Theme',
+   'version': '15.0.0',
+   "license": "Other proprietary",
+   "author": "Your Name",
+   "application": True,
+   "installable": True,
+   "auto_install": False,
+   'depends': ['website'],
+   'data': [
+   ],
+   'assets': {
+       'web._assets_primary_variables': [
+            ('append', 'website_airproof/static/src/scss/primary_variables.scss'),
+        ],
+        'web._assets_frontend_helpers': [
+            ('append', 'website_airproof/static/src/scss/bootstrap_overridden.scss'),
+        ],
+        'web.assets_frontend': [
+            'website_airproof/static/src/scss/theme.scss',
+        ],
+   },
+}
